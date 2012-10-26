c9211754d81698640db018c195fab148127de05e
rhlobo <rhlobo+github@gmail.com>
2012-10-26 16:59:10 -0200

    Organized the experiments module

--- stockExperiments/experiments.py
+++ stockExperiments/experiments.py
@@ -6,30 +6,32 @@ from matplotlib import pyplot
 from matplotlib.dates import date2num
 
 
-def plotstockdata(stockdata, stockticker, dates, col):
-    pyplot.plot_date(stockdata[0], stockdata[1], '-', xdate=True)
-    pyplot.title('%s - %s / %s' % (stockticker, col, dates))
-    pyplot.xlabel(dates)
-    pyplot.ylabel(col)
-    pyplot.savefig('output/%s.png' % stockticker)
+def main():
+    s_symbol = "PETR4"
+    plot(loadData(s_symbol), s_symbol, "Date", "Cotation")
+
+
+def loadData(s_symbol):
+    d_data = providers.get().load(s_symbol)
+    quotes, dates = [[], []]
+    for date in sorted(d_data):
+        dates.append(date2num(date))
+        quotes.append(d_data[date][4])
+    return [dates, quotes]
+
+
+def plot(ll_data, s_symbol, s_xAxDesc, s_yAxDesc):
+    pyplot.plot_date(ll_data[0], ll_data[1], '-', xdate=True)
+
+    pyplot.title('%s - %s / %s' % (s_symbol, s_yAxDesc, s_xAxDesc))
+    pyplot.xlabel(s_xAxDesc)
+    pyplot.ylabel(s_yAxDesc)
+    pyplot.savefig('output/%s.png' % s_symbol)
     pyplot.show()
 
 
 if __name__ == '__main__':
-    s_symbol = "PETR4"
-    d = providers.get().load(s_symbol)
-
-    l_date = sorted(d)
-    quotes = []
-    dates = []
-    for date in l_date:
-        i_date = date2num(date)
-        dates.append(i_date)
-        quotes.append(d[date][4])
-        print (i_date, d[date][4])
-    data = [dates, quotes]
-    plotstockdata(data, s_symbol, "Date", s_symbol)
-
+    main()
 
 # TODO Logging
 # TODO Remove AbstractProvider ?
