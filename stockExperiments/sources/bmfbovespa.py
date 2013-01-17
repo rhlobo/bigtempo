15bbbceb0e68178909455b0b2e028527e39e5fb2
rhlobo <rhlobo+github@gmail.com>
2013-01-17 14:17:01 -0200

    Fixed bmfbovespa call method

--- stockExperiments/sources/bmfbovespa.py
+++ stockExperiments/sources/bmfbovespa.py
@@ -6,7 +6,9 @@ class SymbolImporter(object):
 
     def __init__(self):
         self.regex = re.compile(r'<span\s+id="ctl00_contentPlaceHolderConteudo_grdResumoCarteiraTeorica_ctl00_ctl\d+_lblCodigo".*?>([\w\d]+)</span>', re.IGNORECASE | re.DOTALL | re.MULTILINE)
-        self.s_urlTemplate = 'http://www.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?Indice=%s&idioma=pt-br'
+        self.s_urlTemplate = 'http://www.bmfbovespa.com.br/indices/ResumoCarteiraTeorica.aspx?=%s&idioma=pt-br'
 
     def import_symbols(self, s_index):
-        return re.findall(self.regex, self.s_urlTemplate % (s_index))
+        data = urllib.urlencode({"Indice": s_index})
+        s_content = urllib.urlopen(self.s_urlTemplate % data)
+        return re.findall(self.regex, s_content)
