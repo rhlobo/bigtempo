7b3351116ec57575d9b871f73f78cadc9a51cf14
rhlobo <rhlobo+github@gmail.com>
2013-03-29 20:39:09 -0300

    Created notebook about dataframe import and export (to and form) cvs files.

--- stockExperiments/configurations.py
+++ stockExperiments/configurations.py
@@ -2,5 +2,10 @@ import os
 import datetime
 
 
+def _determine_data_dir():
+    data_dir = os.path.abspath('sources/data')
+    return data_dir if not 'ipy-notebooks' in data_dir else os.path.abspath('../stockExperiments/sources/data')
+
+
 START_DATE = datetime.date(2000, 1, 1)
-DATA_DIR = os.path.abspath('sources/data')
+DATA_DIR = _determine_data_dir()
