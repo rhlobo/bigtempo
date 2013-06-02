e8bce24625fb2ab94984c49321a7b50c0e831cfe
rhlobo <rhlobo+github@gmail.com>
2013-06-02 18:20:48 -0300

    Improved test runner: now displaying pep8 style checking.

--- bin/test.sh
+++ bin/test.sh
@@ -3,6 +3,22 @@ CURRDIR="$(pwd)"
 TESTTYPE=${1:-full}
 export TESTTYPE
 cd "${PROJECT_HOME}/stockExperiments"
+
+
+echo
+echo "UNIT TESTS"
+echo "--------------------------------------------------------------------------------"
 nosetests -v --with-coverage --cover-erase --cover-inclusive --cover-branches --cover-package=stockExperiments --cover-html --cover-html-dir="../coverage/" -w stockExperiments
+echo
+echo
+echo "PEP8 CODE STYLE CHECK"
+echo "--------------------------------------------------------------------------------"
+find "stockExperiments" -type f -name "*.py" -exec pep8 --first --max-line-length=160 {} \;
+echo
+echo "#     Err  Description"
+echo "--------------------------------------------------------------------------------"
+pep8 --statistics --max-line-length=160 -qq "./stockExperiments" | sort -nr
+echo
+
 find . -type f -name "*.pyc" -exec rm -f {} \;
 cd "${CURRDIR}"
