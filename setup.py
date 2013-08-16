19745962560a8d0eb90810b0f48369a4037cbb9f
rhlobo <rhlobo+github@gmail.com>
2013-08-15 23:18:44 -0300

    Created backlog; Created sketch of the content that has to be created for the project; Created setup.py for installation;

--- setup.py
+++ setup.py
@@ -1,39 +1,65 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Filename: setup.py
 
 
-from setuptools import setup, find_packages
 import os
+import sys
+from pkgutil import walk_packages
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def find_packages(path=__path__, prefix=""):
+    yield prefix
+    prefix = prefix + "."
+    for _, name, ispkg in walk_packages(path, prefix):
+        if ispkg:
+            yield name
 
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+
+import bigtempo
+
+
 setup(
-    name="bigtempo",
-    version="0.2",
-    packages=find_packages(exclude=["*.tests",
-                                    "*.tests.*",
-                                    "tests.*",
-                                    "tests"]),
-    author="Roberto Haddock Lobo",
-    author_email="rhlobo+bigtempo@gmail.com",
-    description="bigtempo",
+    name='bigtmepo',
+    version=bigtempo.__version__,
+    description='Powerful processment of temporal data.',
     long_description=read('README.md'),
-    license="MIT",
-    #url='http://pypi.python.org/pypi/bigtempo',
-    keywords=["investment", "financial", "stock"],
-    classifiers=[
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Development Status :: 4 - Beta",
-        "Environment :: Other Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Financial and Insurance Industry",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: Office/Business :: Financial :: Investment",
-    ],
+    author='Roberto Haddock Lobo',
+    author_email='rhlobo+bigtempo@gmail.com',
+    #url='http://',
+    packages=list(find_packages(bigtempo.__path__, bigtempo.__name__)),
+    package_data={'': ['LICENSE']},
+    package_dir={'bigtempo': 'bigtempo'},
+    include_package_data=True,
+    install_requires=[],
+    license=read('LICENSE'),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Financial and Insurance Industry',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    )
 )
