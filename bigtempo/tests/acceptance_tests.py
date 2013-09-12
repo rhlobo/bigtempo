3bba8eae0a51be37311aa3788c8594458af83c78
rhlobo <rhlobo+github@gmail.com>
2013-09-12 10:53:36 -0300

    Partial commit: Improved bin scripts; Acceptance test on the go.

--- bigtempo/tests/acceptance_tests.py
+++ bigtempo/tests/acceptance_tests.py
@@ -19,20 +19,45 @@ def _get_test_data_file(reference, symbol):
     return ''
 
 
-@engine.datasource('SAMPLE_SOURCE_1',
-                   tags=['SAMPLE_INPUT'])
-class Sample1(object):
+@engine.datasource('SAMPLE',
+                   tags=['SAMPLE_IN', 'DAILY'])
+class Sample(object):
 
     def evaluate(self, context, symbol, start=None, end=None):
         return pandas.read_csv(_get_test_data_file('SAMPLE_SOURCE_1', symbol))
 
 
-@engine.datasource('SAMPLE_SOURCE_2',
-                   tags=['SAMPLE_INPUT'])
-class Sample2(object):
+@engine.datasource('WEEKLY_SAMPLE',
+                   dependencies=['SAMPLE'],
+                   tags=['SAMPLE_IN', 'WEEKLY'],
+                   frequency='W-FRI')
+class Weekly(object):
 
     def evaluate(self, context, symbol, start=None, end=None):
-        return pandas.read_csv(_get_test_data_file('SAMPLE_SOURCE_2', symbol))
+        return context.dependencies('SAMPLE').resample('W-FRI', how=lambda x: x[-1])
+
+
+@engine.datasource('MONTHLY_SAMPLE',
+                   dependencies=['SAMPLE'],
+                   tags=['SAMPLE_IN', 'MONTHLY'],
+                   frequency='BM')
+class Monthly(object):
+
+    def evaluate(self, context, symbol, start=None, end=None):
+        return context.dependencies('SAMPLE').resample('BM', how=lambda x: x[-1])
+
+
+@engine.for_each(engine.select('SAMPLE_IN'))
+def _percentual_change_factory(source_reference):
+
+    @engine.datasource('PERCENTUALT_CHANGE:%s' % source_reference,
+                       dependencies=[source_reference],
+                       lookback=1,
+                       tags=['PERCENTUALT_CHANGE'])
+    class PctChange(object):
+
+        def evaluate(self, context, symbol, start=None, end=None):
+            return context.dependencies(source_reference).pct_change()
 
 
 for symbol in symbols:
